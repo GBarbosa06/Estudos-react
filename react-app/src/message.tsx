292107d0b8d124136341d44db150e553ae48a3b9
GBarbosa <andradegui2006@gmail.com>
2025-02-18 20:20:11 -0300

    Create message.tsx

---
+++ react-app/src/message.tsx
@@ -0,0 +1,11 @@
+/* codigos em typescript usam Pascal Case ou Upper Camel Case
+ *  Substituir espaços por maiúsculas
+ */
+
+function Message(){
+    return (
+        <div>Message</div>
+    ) //esse código que será renderizado não será enviado como HTML, mas sim um componente react que será convertido do HTML
+}
+
+export default Message;
