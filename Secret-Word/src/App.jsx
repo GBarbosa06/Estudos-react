33a6c0cee321f52dc0496eb08f5cb8e541d9d1f0
GBarbosa <andradegui2006@gmail.com>
2025-04-03 17:11:49 -0300

    name fixes

--- Secret-Word/src/App.jsx
+++ Secret-Word/src/App.jsx
@@ -94,9 +94,25 @@ function App() {
 
   return (
     <div className='App'>
-      {gameStage === 'start' && <StartScreen startGame={startGame} />}
-      {gameStage === 'game' && <Game verifyLetter={verifyLetter} pickedWord={pickedWord} pickedCategory={pickedCategory} letters={letters} guessedLetters={guessedLetters} wrongLetters={wrongLetters} guesses={guesses} score={score} />}
-      {gameStage === 'end' && <GameOver retry={retry} />}
+      {gameStage === 'start' && <StartScreen 
+      startGame={startGame} 
+      />}
+
+      {gameStage === 'game' && <Game 
+      verifyLetter={verifyLetter} 
+      pickedWord={pickedWord} 
+      pickedCategory={pickedCategory} 
+      letters={letters} 
+      guessedLetters={guessedLetters}
+      wrongLetters={wrongLetters} 
+      guesses={guesses} 
+      score={score} 
+      />}
+
+      {gameStage === 'end' && <GameOver 
+      retry={retry} 
+      score={score} 
+      />}
       
     </div>
   )
