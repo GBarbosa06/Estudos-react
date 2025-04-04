edda2e6980a9534d5fa80056a49b696a2b16c955
GBarbosa <andradegui2006@gmail.com>
2025-04-04 20:50:20 -0300

    Update App.jsx

--- RequisicoesHTTP/src/App.jsx
+++ RequisicoesHTTP/src/App.jsx
@@ -1,9 +1,38 @@
+import { li } from 'motion/react-client';
 import './App.css'
 
+import { useState, useEffect } from 'react'
+
+const url = "http://localhost:3000/products";
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+    const res = await fetch (url);
+
+    const data = await res.json();
+
+    setProducts(data);
+  }
+  fetchData();
+}, [])
+
+
+
+
   return (
     <div className='App'>
       <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$ {product.price.toFixed(2)}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
