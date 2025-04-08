c6a68d585465832cb831dd6fa18267818e7be94b
GBarbosa <andradegui2006@gmail.com>
2025-04-08 20:03:31 -0300

    Update useFetch.jsx

--- RequisicoesHTTP/src/Hooks/useFetch.jsx
+++ RequisicoesHTTP/src/Hooks/useFetch.jsx
@@ -3,6 +3,22 @@ import { useState, useEffect } from "react";
 export const useFetch = (url) => {
     const [data, setData] = useState(null);
 
+    const [config, setConfig] = useState(null);
+    const [method, setMethod] = useState(null);
+    const [callFetch, setCallFetch] = useState(false);
+
+    const httpConfig = (data, method) => {
+        if (method === "POST") {
+            setConfig({
+                method,
+                headers: {
+                    "Content-type" : "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+        }
+    }
+
     useEffect(() => {
         const fetchData = async () => {
             const res = await fetch(url);
@@ -11,6 +27,20 @@ export const useFetch = (url) => {
         }
         fetchData();
     }, [url])
-    return { data }
+
+    useEffect(() =>{
+        const httpRequest = async () => {
+            if(method==="POST"){
+                let fetchOptions = [url, config];
+    
+                const res = await fetch(...fetchOptions);
+                const json = await res.json();
+                setCallFetch(json);
+            }
+        }
+        httpRequest();
+    }, [config])
+
+    return { data, httpConfig }
 
 }
