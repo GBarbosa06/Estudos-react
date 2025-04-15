5ba1b8a5c425302b714d57c5bc5ba96e0dbe86b0
GBarbosa <andradegui2006@gmail.com>
2025-04-15 20:41:23 -0300

    Consumindo e alterando o context

--- contextAPI/src/pages/Home.jsx
+++ contextAPI/src/pages/Home.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import {useContext} from 'react'
+import {CounterContext} from '../context/CounterContext'
 
 const Home = () => {
+    const {counter, setCounter} = useContext(CounterContext)
+
   return (
-    <div>Home</div>
+    <div>
+        <h1>Home</h1>
+        <h2>Valor do contador: {counter}</h2>
+        <button onClick={() => setCounter(counter+1)}>Clique aqui</button>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
+    </div>
+
   )
 }
 
