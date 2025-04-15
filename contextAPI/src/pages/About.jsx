76ff418b10b3af2fc601607d1a8aa9cf4594ec9b
GBarbosa <andradegui2006@gmail.com>
2025-04-15 20:51:31 -0300

    Contexto em todos as paginas

--- contextAPI/src/pages/About.jsx
+++ contextAPI/src/pages/About.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { useContext } from "react"
+import { CounterContext } from "../context/CounterContext"
 
 const About = () => {
+    const {counter, setCounter} = useContext(CounterContext)
   return (
-    <div>About</div>
+    <div>
+        <h1>Products</h1>
+        <h2>Valor do contador: {counter}</h2>
+        <button onClick={() => setCounter(counter+1)}>Clique aqui</button>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
+    </div>
   )
 }
 
