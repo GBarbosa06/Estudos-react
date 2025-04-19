0b1350860c0c5e8d7eb5723b31873554c26f1449
GBarbosa <andradegui2006@gmail.com>
2025-04-19 13:50:50 -0300

    Refazendo o modulo de router

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -1,17 +1,9 @@
 import './App.css'
 
-import { useNavigate } from 'react-router-dom'
-
-
 function App() {
-  const navigate = useNavigate();
-  const pageChange = () => {
-    navigate('/pag2');
-  }
-  return (
+  return(
     <div>
-      Página principal
-      <button onClick={pageChange}>Pagina 2</button>
+      App
     </div>
   )
 }
--- router/src/main.jsx
+++ router/src/main.jsx
@@ -2,22 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import PaginaDois from './pages/PaginaDois.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/pag2",
-    element: <PaginaDois/>
-  }
-])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
