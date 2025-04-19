01e4a92d682a265fd345b9c0507b61b363ee558e
GBarbosa <andradegui2006@gmail.com>
2025-04-19 15:58:35 -0300

    Update Product.jsx

--- router/src/pages/Product.jsx
+++ router/src/pages/Product.jsx
@@ -1,11 +1,25 @@
 import { useParams } from "react-router-dom"
+import { useFetch } from "../hooks/useFetch"
 
 const Product = () => {
   const { id } = useParams()
+    const url = "http://localhost:3000/products/" + id;
+    const { data: product, loading, error } = useFetch(url)
+
+    console.log(product)
 
     return (
     <div>
         <p>ID do produto: {id}</p>
+        {loading && <p>Carregando...</p>}
+        {error && <p>{error}</p>}
+        {product && (
+            <div>
+                <h2>{product.name}</h2>
+                <p>R$ {product.price}</p>
+                <p>Descrição: {product.description}</p>
+            </div>
+        )}
     </div>
   )
 }
