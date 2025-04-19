02225b6581f70261c30251a9175a5978feeccb9f
GBarbosa <andradegui2006@gmail.com>
2025-04-19 15:52:45 -0300

    Rotas dinâmicas

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/products/:id" element={<Product />} />
         </Routes>
       </BrowserRouter>
     </div>
