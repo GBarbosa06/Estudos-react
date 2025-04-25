c1748fa1e097bf3e13e627f8fb5f1fd5169f67a1
GBarbosa <andradegui2006@gmail.com>
2025-04-25 16:52:08 -0300

    Router + pages + firebase setup

--- MiniBlog/src/App.jsx
+++ MiniBlog/src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>MiniBlog</h1>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="/about" element={<h1>About</h1>} />
+            <Route path="/posts" element={<h1>Posts</h1>} />
+            <Route path="/posts/:id" element={<h1>Post</h1>} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+          <footer>
+            <p>Mini Blog &copy; 2023</p>
+          </footer>
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
