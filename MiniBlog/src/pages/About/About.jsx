a548e96da05687fa9789b3ee25040a6e1f7ad6cc
GBarbosa <andradegui2006@gmail.com>
2025-04-29 17:30:47 -0300

    Update About.jsx

--- MiniBlog/src/pages/About/About.jsx
+++ MiniBlog/src/pages/About/About.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div className="styles.about">
+    <div className={styles.about}>
       <h2>Sobre o Mini <span>Blog</span></h2>
       <p>Este projeto consiste em um blog feito com React no front-end e Firebase no back-end</p>
 
