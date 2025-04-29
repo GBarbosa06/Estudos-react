b8b869ad413d14dd5376ccfd861b39b1803833ac
GBarbosa <andradegui2006@gmail.com>
2025-04-29 15:59:58 -0300

    Show/hide login/reg buttons

--- MiniBlog/src/components/Navbar.jsx
+++ MiniBlog/src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import { NavLink } from "react-router-dom";
 import styles from './Navbar.module.css';
 
+import { useAuthentication } from "../hooks/useAuthentication";
+import { useAuthValue } from "../context/AuthContext";
+
 const Navbar = () => {
+
+    const {user} = useAuthValue();
+    
   return (
     <nav className={styles.navbar}>
         <NavLink to="/" className={styles.logo}>
@@ -13,16 +19,20 @@ const Navbar = () => {
                     Home
                 </NavLink>
             </li>
-            <li>
-                <NavLink to="/login" className={({ isActive }) => (isActive ? styles.active : undefined)}>
-                    Entrar
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to="/register" className={({ isActive }) => (isActive ? styles.active : undefined)}>
-                    Registrar
-                </NavLink>
-            </li>
+            {!user && 
+                <>
+                <li>
+                    <NavLink to="/login" className={({ isActive }) => (isActive ? styles.active : undefined)}>
+                        Entrar
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/register" className={({ isActive }) => (isActive ? styles.active : undefined)}>
+                        Registrar
+                    </NavLink>
+                </li>
+                </>
+            }
             <li>
                 <NavLink to="/about" className={({ isActive }) => (isActive ? styles.active : undefined)}>
                     Sobre
