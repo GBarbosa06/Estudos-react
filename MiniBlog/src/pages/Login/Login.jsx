d9cf1f602f2e6e285a155e6df6ae8cf74a32f7a8
GBarbosa <andradegui2006@gmail.com>
2025-05-02 16:43:45 -0300

    estrutura da página de login

--- MiniBlog/src/pages/Login/Login.jsx
+++ MiniBlog/src/pages/Login/Login.jsx
@@ -1,9 +1,60 @@
 import styles from './Login.module.css'
+import { useState, useEffect } from 'react'
+import { useAuthentication } from '../../hooks/useAuthentication'
+import { useAuthValue } from '../../context/AuthContext'
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const { error: authError, loading} = useAuthentication();
+
+  const user = {
+    email,
+    password
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+
   return (
-    <div>
-        <h2>Login</h2>
+    <div className={styles.login}>
+        <h1>Entrar</h1> 
+        <p>Faça o login para poder utilizar o sistema</p>
+        <p>O cadastro é gratuito e rápido!</p>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Email:</span>
+            <input 
+              type="email" 
+              name="email" 
+              required 
+              placeholder='Email do usuário'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label>
+            <span>Senha:</span>
+            <input 
+              type="password" 
+              name="password" 
+              required 
+              placeholder='Senha do usuário' 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+
+          {!loading && <button className='btn'>Entrar</button>}
+          {loading && <button className='btn' disabled>Aguarde...</button>}
+
+          {error && <p className='error'>{error}</p>}
+          <p>Já tem uma conta? <a href="/login">Clique aqui</a></p>
+        </form>
     </div>
   )
 }
