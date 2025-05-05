1056bd9225587fca6d4f547fbc098bf612ddaff0
GBarbosa <andradegui2006@gmail.com>
2025-05-05 19:37:20 -0300

    Data fetch hook

---
+++ MiniBlog/src/hooks/useFetchDocuments.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+import { 
+    collection, 
+    query,
+    getDocs, 
+    orderBy,
+    onSnapshot,
+    where,
+    QuerySnapshot, 
+ } from "firebase/firestore";
+
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+    const [documents, setDocuments] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(null);
+
+    const [cancelled, setCancelled] = useState(false);
+
+    useEffect(() =>{
+        const loadData = async () =>{
+            if(cancelled) return;
+
+            setLoading(true);
+
+            const collectionRef = await collection(db, docCollection);
+
+            try {
+                let q;
+
+                //search
+
+                //dashboards
+
+                q = await query(collectionRef, orderBy("createdAt", "desc"));
+
+                await onSnapshot(q, (QuerySnapshot) => {
+                    setDocuments(
+                        QuerySnapshot.docs.map((doc) => ({
+                            id: doc.id,
+                            ...doc.data(),
+                        }))
+                    )
+                })
+                setLoading(false);
+            } catch (error) {
+                console.log(error);
+                setError(error.message);
+                setLoading(false);
+            }
+            
+        }
+        loadData();
+    }, [docCollection, search, uid, cancelled]);
+
+    useEffect(() => {
+        return () => setCancelled(true);
+    }, []);
+    return { documents, loading, error };
+};
