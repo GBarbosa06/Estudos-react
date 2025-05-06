61715873f877f09efb17e2223dbf144b645bf8f5
GBarbosa <andradegui2006@gmail.com>
2025-05-05 22:25:54 -0300

    Css fixes

--- MiniBlog/src/components/PostDetail.jsx
+++ MiniBlog/src/components/PostDetail.jsx
@@ -1,26 +1,27 @@
-import styles from './PostDetail.module.css';
+import { Link } from "react-router-dom";
 
-import { Link } from 'react-router-dom';
+import styles from "./PostDetail.module.css";
 
-import React from 'react'
-
-const PostDetail = ({post}) => {
+const PostDetail = ({ post }) => {
+    //! fix css centering
   return (
-    <div>
-        <img src={post.image} alt={post.title} />(
-        <h2>{post.title}</h2>
-        <p className={styles.createdby}>por: {post.createdBy}</p>
-        <div>
-            {post.tags && post.tags.map((tag) => (
-                <p key={tag}>
-                   <span>#</span>
-                   {tag}
-                </p>
-            ))}
-        </div>
-        <Link to={`/posts/${post.id}`} className='btn btn-outline'>Ler</Link>
+    <div className={styles.post_detail}>
+      <img src={post.image} alt={post.title} />
+      <h2>{post.title}</h2>
+      <p className={styles.createdby}>por: {post.createdBy}</p>
+      <div className={styles.tags}>
+        {post.tags.map((tag) => (
+          <p key={tag}>
+            <span>#</span>
+            {tag}
+          </p>
+        ))}
+      </div>
+      <Link to={`/posts/${post.id}`} className="btn btn-outline">
+        Ler
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default PostDetail
+export default PostDetail;
