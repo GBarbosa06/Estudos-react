f06b8dabfbf11479831af0494825a98f0ceb3c72
GBarbosa <andradegui2006@gmail.com>
2025-05-06 15:16:50 -0300

    Update CreatePost.jsx

--- MiniBlog/src/pages/CreatePost/CreatePost.jsx
+++ MiniBlog/src/pages/CreatePost/CreatePost.jsx
@@ -109,6 +109,7 @@ const CreatePost = () => {
             Aguarde.. .
           </button>
         )}
+        <button className={`btn ${styles.btnCancel}`} onClick={() => {navigate("/")}}>Cancelar</button>
         {(response.error || formError) && (
           <p className="error">{response.error || formError}</p>
         )}
