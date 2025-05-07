f5bb9c99cf423e71a7e4791578de9aa76de15007
GBarbosa <andradegui2006@gmail.com>
2025-05-07 19:48:37 -0300

    post page complete + css

--- MiniBlog/src/pages/Post/Post.jsx
+++ MiniBlog/src/pages/Post/Post.jsx
@@ -8,11 +8,20 @@ const Post = () => {
     const {document: post, loading} = useFetchDocument("posts", id);
 
   return (
-    <div>
+    <div className={styles.post_container}>
         {loading && <p>Carregando...</p>}
         {post && (
             <>
-            <h1>{post.title}</h1></>
+              <h1>{post.title}</h1>
+              <img src={post.image} alt={post.title} />
+              <p>{post.body}</p>
+              <h3>Este post trata sobre:</h3>
+              {post.tags.map((tag) => (
+                <p key={tag} className={styles.tags}><span>#</span>{tag}</p>
+              ))}
+              {post.createdBy && <h3>Post de <span>{post.createdBy}</span></h3>
+              }
+            </>
         )}
     </div>
   )
